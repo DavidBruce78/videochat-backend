4f57263a023d479640e21f69925d7b6006bbca2f
David Bruce <davidbruce78@gmail.com>
2025-05-23 20:00:57 -0400

    🔐 Load Firebase admin key from env var

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const bodyParser = require('body-parser');
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
